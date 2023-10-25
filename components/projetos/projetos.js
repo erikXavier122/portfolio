983ee74e76f346e709d550955a3a90e7f4ee1a99
Breno Kevin <brenosteams@gmail.com>
2023-10-24 22:57:51 -0300

    projetos

--- components/projetos/projetos.js
+++ components/projetos/projetos.js
@@ -4,25 +4,53 @@ tituloProjetos.innerText = "Projetos"
 
 pageProjetosGetById.appendChild(tituloProjetos)
 
-function createButtonsprojects(id,text){
+function createButtonsProjects(id,href){
     const buttonProjetos = document.createElement('button')
     buttonProjetos.id = id
-    buttonProjetos.innerText = text
+    buttonProjetos.href = href
     pageProjetosGetById.appendChild(buttonProjetos)
 }
-createButtonsprojects("btnUsuarios", "Cadastro de usuarios")
-createButtonsprojects("btnEmpresa", "Cadastro de empresas")
-createButtonsprojects("btnProdutos", "Cadastro de produtos")
-createButtonsprojects("btnUsers", "Padastro de usuario")
-createButtonsprojects("btnlogin", "Página de login")
-createButtonsprojects("pageCadastroFront", "Página de usuarios")
-createButtonsprojects("btnPortfolio", "Meu portfolio")
-createButtonsprojects("btnPicpay","Desafio PicPay")
+createButtonsProjects("btnUsuarios", "https://github.com/erikXavier122/spring-boot-api-cadastro")
+createButtonsProjects("btnEmpresa", "https://github.com/erikXavier122/spring-boot-api-empresa.git")
+createButtonsProjects("btnProdutos","https://github.com/erikXavier122/spring-boot-api-product.git")
+createButtonsProjects("btnUsers", "https://github.com/erikXavier122/spring-boot-api-user.git")
+createButtonsProjects("btnLoginFront","https://github.com/erikXavier122/pagina-de-login.git")
+createButtonsProjects("btnCadastroFront","https://github.com/erikXavier122/Cadastro_de_usuarios.git")
+createButtonsProjects("btnPortfolio","https://github.com/erikXavier122/portfolio.git")
+createButtonsProjects("btnPicpay","https://github.com/erikXavier122/picpay.git")
 
-const picpayGetById = document.getElementById("btnPicpay")
+function createimgProjects(id,src,idDestino){
+    const imgProjetos = document.createElement('img')
+    imgProjetos.id = id
+    imgProjetos.src = src
+    console.log(imgProjetos)
+    const insideBtn = document.getElementById(idDestino)
+    console.log(insideBtn)
+    insideBtn.appendChild(imgProjetos)
 
-const imgPicPay = document.createElement("img")
-imgPicPay.className = "imgPicPay"
-imgPicPay.src = "./assets/principal-page/icons8-loading-100.png"
+}
+createimgProjects("imgUsuarios","assets/principal-page/icons8-loading-100.png","btnUsuarios")
+createimgProjects("imgEmpresa","./assets/principal-page/icons8-loading-100.png","btnEmpresa")
+createimgProjects("imgProdutos","./assets/principal-page/icons8-loading-100.png","btnProdutos")
+createimgProjects("imgUsers","./assets/principal-page/icons8-loading-100.png","btnUsers")
+createimgProjects("imgLogin","./assets/principal-page/icons8-loading-100.png","btnLoginFront")
+createimgProjects("imgPageCadastro","./assets/principal-page/icons8-loading-100.png","btnCadastroFront")
+createimgProjects("imgPortfolio","./assets/principal-page/icons8-loading-100.png","btnPortfolio")
+createimgProjects("imgPicpay","./assets/principal-page/icons8-loading-100.png","btnPicpay")
 
-picpayGetById.appendChild(imgPicPay)
+function createTextProjects(id,text,idTextDestino){
+    const textProjetos = document.createElement('h2')
+    textProjetos.id = id
+    textProjetos.innerText = text
+    const insideTextBtn = document.getElementById(idTextDestino)
+    insideTextBtn.appendChild(textProjetos)
+
+}
+createTextProjects("textUsuarios","...","btnUsuarios")
+createTextProjects("textEmpresa","...","btnEmpresa")
+createTextProjects("textProdutos","...","btnProdutos")
+createTextProjects("textUsers","...","btnUsers")
+createTextProjects("textLogin","...","btnLoginFront")
+createTextProjects("textPageCadastro","...","btnCadastroFront")
+createTextProjects("textPortfolio","...","btnPortfolio")
+createTextProjects("textPicpay","...","btnPicpay")
